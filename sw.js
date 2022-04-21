8fc00682d0a52b7fdecab36ba49f8df3f8dd066c
Umar Abdul <aufaruq02@gmail.com>
2022-04-21 11:32:18 +0100

    Updated blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220409.1537';
+const cacheName = 'chirpy-20220421.1126';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
